2b97108149e22635608b57a1ddf5677a9247859e
JordanG <26260@ma-web.nl>
2022-07-06 00:42:13 +0200

    tile rendering added

--- Scripts/Game.js
+++ Scripts/Game.js
@@ -4,6 +4,37 @@ class Game {
     this.height = height;
     this.renderScale = renderScale;
     this.player = new Player(this);
+    this.world = {
+
+      map: ["000", "000", "000", "000", "000", "000", "000",
+            "000", "000", "000", "000", "000", "000", "000",
+            "000", "000", "000", "000", "000", "000", "000",
+            "000", "000", "000", "000", "000", "000", "000",
+            "131", "132", "133", "134", "135", "136", "137",
+            "152", "152", "152", "152", "152", "152", "152"
+      ],
+
+      colums: 7,
+      sheetColums: 13,
+      tileWidth: 96,
+      yOffset: 840,
+      sheetSrc: "Img/Tilesets/nghz1/nghz1Sheet.png"
+    }
+
+    this.chunks = [];
+
+    for (var i = 0; i < this.world.map.length; i++) {
+      let chunkString = this.world.map[i];
+
+      if (chunkString !== "000") {
+        let destinationX = (i % this.world.colums) * (this.world.tileWidth * this.renderScale);
+        let destinationY = Math.floor(i / this.world.colums) * (this.world.tileWidth * this.renderScale);
+
+        this.chunks.push(new Chunk(destinationX, destinationY - this.world.yOffset, this.world.sheetSrc, parseInt(chunkString), this.world.sheetColums, this.world.tileWidth, this.renderScale));
+      }
+    }
+
+    console.log(this.chunks);
   }
 
   update() {
@@ -11,6 +42,12 @@ class Game {
   }
 
   draw(ctx) {
+
+    for (var i = 0; i < this.chunks.length; i++) {
+      let chunk = this.chunks[i];
+      chunk.draw(ctx);
+    }
+
     this.player.draw(ctx);
     this.player.drawDebug(ctx);
   }
--- Scripts/chunk.js
+++ Scripts/chunk.js
@@ -1,9 +1,22 @@
 class Chunk {
-  constructor(x, y, src, chunkIndex) {
-    this.x = x;
-    this.y = y;
+  constructor(renderX, renderY, src, chunkIndex, tileSheetColums, tileWidth, renderScale) {
+    this.x = 0;
+    this.y = 0;
+    this.renderX = renderX;
+    this.renderY = renderY;
+    this.width = tileWidth;
+    this.sourceX = (chunkIndex % tileSheetColums) * tileWidth;
+    this.sourceY = Math.floor(chunkIndex / tileSheetColums) * tileWidth;
     this.chunkIndex = chunkIndex;
     this.spritesheet = new Image();
     this.spritesheet.src = src;
+    this.renderScale = renderScale;
+  }
+
+  draw(ctx) {
+    //ctx.drawImage(this.spritesheet, this.renderX, this.renderY, this.width * this.renderScale);
+    ctx.drawImage(this.spritesheet, this.sourceX, this.sourceY,
+      this.width, this.width, this.renderX, this.renderY,
+      this.width * this.renderScale, this.width * this.renderScale);
   }
 }
